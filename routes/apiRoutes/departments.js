5d4bd97c6836defbae9227676def36720f996001
lorreina <slorreina369@gmail.com>
2022-07-27 16:44:20 -0700

    departments post function created and working

--- routes/apiRoutes/departments.js
+++ routes/apiRoutes/departments.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router('');
 const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
 
 router.get('/departments', (req,res) =>{
     const sql = 'SELECT * FROM departments';
@@ -50,6 +51,37 @@ router.delete('/departments/:id', (req,res) =>{
                 id:req.params.id
             });
         }
+    });
+});
+
+router.post('/departments', ({body}, res)=>{
+    const errors = inputCheck(
+        body,
+        'id',
+        'name'
+    );
+
+    if(errors){
+        res.status(400).json({error:errors});
+        return;
+    }
+
+    const sql = `INSERT INTO departments(id, name) VALUES (?,?)`;
+    const params =[
+        body.id,
+        body.name
+    ];
+
+    db.query(sql,params, (err,result) =>{
+        if(err){
+            res.status(400).json({error:err.message});
+            return;
+        }
+        res.json({
+            message:'success',
+            data:body,
+            changes: result.affectedRows
+        });
     });
 });
 
